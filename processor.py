61abed9f8d3ddd7e9dcf75f4f6f63fefbf99966a
Scarliere (Laptop) <chunee.03@live.com>
2021-06-14 14:41:29 +0800

    Updated for error catching in case of dropped connection

--- processor.py
+++ processor.py
@@ -29,21 +29,29 @@ def processpage(url,i,subber,nextepi,resolution,animename):
 	downloadprefix = 'https://nyaa.si'
 	word = pros.obtainjson(url).text
 	output_json = html_to_json.convert(word)
-	anime_rows = len(output_json['html'][0]['body'][0]['div'][0]['div'][0]['table'][0]['tbody'][0]['tr'])
-	print(url)
-	for i in range(anime_rows):
-		anime_selection = output_json['html'][0]['body'][0]['div'][0]['div'][0]['table'][0]['tbody'][0]['tr'][i]
-		temp_title = anime_selection['td'][1]['a'][0]['_attributes']['title']
-		if(temp_title.find('comments')!=-1 or temp_title.find('comment')!=-1):
-			temp_title = anime_selection['td'][1]['a'][1]['_attributes']['title']
-		if(temp_title.find(subber)!=-1 and temp_title.find(str(nextepi)+' ')!=-1 and temp_title.find(resolution)!=-1):
-			temp_download = downloadprefix+anime_selection['td'][2]['a'][0]['_attributes']['href']
-			print(temp_title)
-			print(temp_download)
-			r = requests.get(temp_download, allow_redirects=True)
-			open(temp_title+'.torrent', 'wb').write(r.content)
-			print('\n')
-			return True
-	print('\n')
-	return False
-	
+	return getrows(output_json,subber,nextepi,resolution,downloadprefix)
+def getrows(output_json,subber,nextepi,resolution,downloadprefix):
+	try:
+		if(nextepi<10):
+			nextepi = '0'+str(nextepi)
+		else: nextepi = str(nextepi)
+		anime_rows = len(output_json['html'][0]['body'][0]['div'][0]['div'][0]['table'][0]['tbody'][0]['tr'])
+		for i in range(anime_rows):
+			anime_selection = output_json['html'][0]['body'][0]['div'][0]['div'][0]['table'][0]['tbody'][0]['tr'][i]
+			temp_title = anime_selection['td'][1]['a'][0]['_attributes']['title']
+			if(temp_title.find('comments')!=-1 or temp_title.find('comment')!=-1):
+				temp_title = anime_selection['td'][1]['a'][1]['_attributes']['title']
+			if(temp_title.find(subber)!=-1 and temp_title.find(nextepi+' ')!=-1 and temp_title.find(resolution)!=-1):
+				temp_download = downloadprefix+anime_selection['td'][2]['a'][0]['_attributes']['href']
+				print(temp_title)
+				print(temp_download)
+				r = requests.get(temp_download, allow_redirects=True)
+				open(temp_title+'.torrent', 'wb').write(r.content)
+				print('\n')
+				return True
+		print('\n')
+		return False
+	except Exception:
+		time.sleep(5)
+		print('Error')
+		return getrows(output_json,subber,nextepi,resolution,downloadprefix)
